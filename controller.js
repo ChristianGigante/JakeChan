092ce5a01234e0af534783977ae941393ef0ff37
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-09-11 21:43:52 +0800

    initial commit

--- controller.js
+++ controller.js
@@ -1,31 +1,13 @@
 $(document).ready(function () {
 
-    $('p').hide();
-
-
-    // function addRow() {
-    //     var tr = $("<tr>")
-    //     $("<td>").text(countryName).appendTo($(tr))
-    //     $("<td>").text("<img src='+" + countryflag + ">").appendTo($(tr))
-    //     $("<td>").text(countryCode).appendTo($(tr))
-    //     $("<td>").text(population).appendTo($(tr))
-    //     $("tbody").append($(tr))
-
-
-    // }
-
-
-
-
     $('#btnSearch').click(function () {
         var search = $('#search').val().toLowerCase();
         var url = "https://restcountries.eu/rest/v2/name/" + search + ""
 
-
         $(document).ajaxStart(function () {
-            $('#btnSearch').attr('disable',true);
-            $('#btnSearch').text('');
-            $('#btnSearch').text('Searching');
+            $('#btnSearch').text('Searching..,');
+            $('#btnSearch').attr('disable', true);
+            emptyTable();
         })
 
         $(document).ajaxStop(function () {
@@ -33,19 +15,17 @@ $(document).ready(function () {
             $('#btnSearch').text('Search');
         })
 
-
         $.getJSON(url, function (data) {
             $.each(data, function (key, val) {
                 if (val.name.toLowerCase().substring(0, search.length) == search) {
-                    $('tbody').append(
-                        "<tr><td>" + val.name + "</td><td><img style='height:20px' src=" + val.flag + "></td><td>" + val.alpha3Code + "</td><td>" + val.population + "</td></tr>"
-                    )
+                    counter++;
+                    addRow(val.name, val.flag, val.alpha3Code, val.population);
                 }
 
             });
 
         });
-        emptyTable();
+
     })
 
 });
